81ff9548e97c81d041db9374eaaed971475452a5
j-custom <itskillup0308-git@yahoo.co.jp>
2023-07-14 05:32:12 +0900

    20230714

--- arg/out.c
+++ arg/out.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
-#include "header.h"
+#include <stdlib.h>
+/*#include "header.h"
+ */
 
 int
 main(int argc, char *argv[])
 {
+  if(argc != 5){
+    exit(1);
+  }
   printf("%d\n", argc);
-  output((int)argv[1], (int)argv[2], (int)argv[3], (int)argv[4]);
+  printf("%s\n", argv[0]);
+  printf ("%d %d %d %d %d\n", argnv[1], argv[2], argv[3], argv[4]);
  return 0;
 }
