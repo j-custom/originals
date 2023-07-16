3824ad974530fe2f55b64ff7cadcc0cd3486d81e
j-custom <itskillup0308-git@yahoo.co.jp>
2023-07-16 10:22:51 +0900

    20230716

--- arg/out.c
+++ arg/out.c
@@ -13,7 +13,10 @@ main(int argc, char *argv[])
   printf("%s\n", argv[0]);
   //  printf ("%d %d %d %d\n", (int)*argv[1], (int)*argv[2], (int)*argv[3], (int)*argv[4]);
   printf ("%s %s %s %s\n", argv[1], argv[2], argv[3], argv[4]);
-  printf ("a+b+c+d = %d\n", argv[1] + argv[2] + argv[3] + argv[4]);
+  printf ("%p %p %p %p\n", argv[1], argv[2], argv[3], argv[4]);
+  printf ("%d %d %d %d\n", (int)*argv[1], (int)*argv[2], (int)*argv[3], (int)*argv[4]);
+
+  //  printf ("a+b+c+d = %d\n", argv[1] + argv[2] + argv[3] + argv[4]);
 
   return 0;
 }
